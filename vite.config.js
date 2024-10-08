5c5f5a2ae11580eebb77831bca47097dc2eccd0e
Shreyash Wagh <shreyashwagh22@gmail.com>
2024-10-08 12:48:34 +0530

    Project Completed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,21 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Set this as needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) {
+              return 'firebase';
+            } else if (id.includes('react')) {
+              return 'react-vendors';
+            } 
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
